67e29f31c9b2b78f97147b9df11dfa5021471b5f
kaverndsp <kaverndsp@users.noreply.github.com>
2020-02-18 15:55:31 -0600

    adding redux boilerplate

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
 import React from 'react';
 
+import {connect} from "react-redux";
+
 import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
 
-const App = () => {
-  const state = {
-    additionalPrice: 0,
-    car: {
-      price: 26395,
-      name: '2019 Ford Mustang',
-      image:
-        'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
-      features: []
-    },
-    additionalFeatures: [
-      { id: 1, name: 'V-6 engine', price: 1500 },
-      { id: 2, name: 'Racing detail package', price: 1500 },
-      { id: 3, name: 'Premium sound system', price: 500 },
-      { id: 4, name: 'Rear spoiler', price: 250 }
-    ]
-  };
-
+const App = (props) => {
+  
+  // Set up "empty" reducer and initial state 👉 Set up store and Provider 👉
+  // Connect components 👉 Add events and event handlers in UI 👉 Build action creators 👉
+  // write the reducer logic for the actions 👉 Rinse and repeat
+ 
+  
   const removeFeature = item => {
     // dispatch an action here to remove an item
   };
@@ -45,4 +36,8 @@ const App = () => {
   );
 };
 
-export default App;
+const mapStateToProps = () => {
+  
+}
+
+export default connect(mapStateToProps, {})(App);
